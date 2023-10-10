5f2ade808c2f2970917371b58f713f4b04ba3c09
Heachy <2582085906@qq.com>
2023-10-10 16:08:51 +0800

    modify

--- 102101227/pairWork/front/es_work/src/api/barrage/getBarrage.js
+++
@@ -1,9 +0,0 @@
-import MyRequest from '../config/config'
-
-const getBarrageApi = () => {
-    return MyRequest({
-        url: '/barrage/get',
-        method: 'get',
-    })
-}
-export default getBarrageApi
--- 1021012272/pairWork/front/es_work/src/api/match/connect.js
+++
@@ -1,9 +0,0 @@
-import MyRequest from '../config/config'
-
-const connectRoom = () => {
-    return MyRequest({
-        url: '/match/connect',
-        method: 'get',
-    })
-}
-export default connectRoom
